577b927b72823b8fb911d3e1ce6ccd91e43c64ef
T Jessop <tjessop602@gmail.com>
2022-09-20 18:06:33 +1200

    Added keyboard functionality.

--- javascript.js
+++ javascript.js
@@ -13,7 +13,7 @@ const lowerScreen = document.getElementById("lower");
 const charLimit = 12;
 
 var upperScreenDisplay = '';
-var lowerScreenDispaly = '';
+var lowerScreenDisplay = '';
 var storedNumber = '';
 var op = nothing;
 
@@ -28,12 +28,12 @@ window.addEventListener('click',(event) => {
 })
 
 updateScreen = function(){
-    lowerScreen.textContent = lowerScreenDispaly;                                               
+    lowerScreen.textContent = lowerScreenDisplay;                                               
     upperScreen.textContent = upperScreenDisplay;
 }
 
 operate = function(pressedEquals = false){
-    let answer = (op(Number(storedNumber), Number(lowerScreenDispaly)));                            //perform whatever operation is stored in op
+    let answer = (op(Number(storedNumber), Number(lowerScreenDisplay)));                            //perform whatever operation is stored in op
     let nBeforeDecimal = (answer.toFixed(0)).length;                         
     answer = String(parseFloat((answer).toFixed(Math.max(0,charLimit-nBeforeDecimal-1))));          //round decimals to fit to screen
     if(answer.length > charLimit){
@@ -42,14 +42,14 @@ operate = function(pressedEquals = false){
         if(pressedEquals && lastPressEquals == true){
             return;                                                                                //Equals was previously pressed and was pressed again. Do nothing.
         }else if(pressedEquals){
-            setEquation(storedNumber, getOpSymbol(op), lowerScreenDispaly, pressedEquals);          //if equals was pressed display answer below and upperScreenDisplay above and set eq state.
-            lowerScreenDispaly = answer;
+            setEquation(storedNumber, getOpSymbol(op), lowerScreenDisplay, pressedEquals);         //if equals was pressed display answer below and upperScreenDisplay above and set eq state.
+            lowerScreenDisplay = answer;
             storedNumber = answer;                                                                 //we need this is user wants to use answer as input for next calculation
             lastPressEquals = true;
         }else{
             storedNumber = answer;                                                                 //if another op button was pressed store input, update upperScreenDisplay with partial answer and await the next number
-            lowerScreenDispaly = '';  
-            setEquation(storedNumber, getOpSymbol(op), lowerScreenDispaly, pressedEquals);
+            lowerScreenDisplay = '';  
+            setEquation(storedNumber, getOpSymbol(op), lowerScreenDisplay, pressedEquals);
         }
         op = nothing;
     }
@@ -57,7 +57,7 @@ operate = function(pressedEquals = false){
 
 error = function(msg){
     upperScreenDisplay = "ERROR";                                                                   //replace screen display with error message and set errorState
-    lowerScreenDispaly = msg;
+    lowerScreenDisplay = msg;
     errorState = true;
     updateScreen();                                                                                 //since we are in errorState, screen must be updated manually.
 }
@@ -66,13 +66,13 @@ clearCurrent = function(){
     if(lastPressEquals){
         clearAll();
     }else{
-        lowerScreenDispaly = '';
+        lowerScreenDisplay = '';
     }
 }
 
 clearAll = function(){ 
     storedNumber = '';
-    lowerScreenDispaly = ''; 
+    lowerScreenDisplay = ''; 
     upperScreenDisplay = '';
     op = nothing;
     errorState = false;
@@ -81,29 +81,29 @@ clearAll = function(){
 }
 
 store = function(){
-    storedNumber = lowerScreenDispaly;
-    lowerScreenDispaly = '';
+    storedNumber = lowerScreenDisplay;
+    lowerScreenDisplay = '';
 }
 
 setOperator = function(func){
     lastPressEquals = false;                                                                        //equals was pressed but user wants to use the answer as input for the next calculation. 
     if(storedNumber == ''){                                                                         //if we don't have a stored value, store input and await further input
         store();
-    }else if(lowerScreenDispaly != ''){                                                              //If we have all the information to perform a calculation then we should do it before changing the operator
-        operate();                                                                                  //Note: if equals was pressed the stored number is the previous answer, otherwise it is whatever the user input.
+    }else if(lowerScreenDisplay != ''){                                                              //If we have all the information to perform a calculation then we should do it before changing the operator
+        operate();                                                                                              //Note: if equals was pressed the stored number is the previous answer, otherwise it is whatever the user input.
     }
     op = func;                                                                                      //Set the operator
-    setEquation(storedNumber, getOpSymbol(op), lowerScreenDispaly);
+    setEquation(storedNumber, getOpSymbol(op), lowerScreenDisplay);
 }
 
 setSign = function(){
     if(lastPressEquals){                                                                             //if this function is called while lastPressEquals, start a new calculation.
         clearAll();
     }
-    if(lowerScreenDispaly[0] != '-'){                                                                //if a - sign is not present, add one.
-        lowerScreenDispaly = '-' + lowerScreenDispaly;
+    if(lowerScreenDisplay[0] != '-'){                                                                //if a - sign is not present, add one.
+        lowerScreenDisplay = '-' + lowerScreenDisplay;
     }else{
-        lowerScreenDispaly = lowerScreenDispaly.slice(1);                                             //else if a - sign is present, remove it.
+        lowerScreenDisplay = lowerScreenDisplay.slice(1);                                            //else if a - sign is present, remove it.
     }
 }
 
@@ -111,15 +111,15 @@ buildNumber = function(digit){
     if(lastPressEquals){                                                                                //if this function is called while lastPressEquals, start a new calculation.
         clearAll();
     }
-    if(lowerScreenDispaly.length >= charLimit || (digit == '.' && lowerScreenDispaly.includes('.'))){   //only add digits if we have space. This isn't specially indicated to the user since it should be obvious
-        return;                                                                                         //that no further numbers are being added. Maybe add some indication?
+    if(lowerScreenDisplay.length >= charLimit || (digit == '.' && lowerScreenDisplay.includes('.'))){   //only add digits if we have space. This isn't specially indicated to the user since it should be obvious
+        return;                                                                                                 //that no further numbers are being added. Maybe add some indication?
     }else{
-        lowerScreenDispaly += digit;   
+        lowerScreenDisplay += digit;   
     }
 }
 
 deleteDigit = function(){
-    lowerScreenDispaly = lowerScreenDispaly.slice(0,-1);
+    lowerScreenDisplay = lowerScreenDisplay.slice(0,-1);
 }
 
 setEquation = function(a = '', symbol = '', b = '', showEquals = false){ 
@@ -139,8 +139,29 @@ getOpSymbol = function(func){
     return '';
 }
 
+getOpName = function(opSymbol){
+    if(opSymbol == '+'){return add};
+    if(opSymbol == '-'){return subtract};
+    if(opSymbol == '*'){return multiply};
+    if(opSymbol == '/'){return divide};
+    return nothing;
+}
+
+window.addEventListener("keypress", function(e) {                                                        //keyboard input functionality
+    if (e.key >= "0" && e.key <= "9"){
+        buildNumber(e.key);
+    }else if(e.key == '-' && (lowerScreenDisplay == '' || lowerScreenDisplay == '-')){                  //since - means both minus and negative we have so ambiguity. We will take it as negative if
+        setSign();                                                                                          //the lower screen is blank or contains only a - sign and minus in all other cases.
+    }else if(e.key == '+' || e.key == '-' || e.key == '*' || e.key == '/'){
+        setOperator(getOpName(e.key));
+    }else if(e.key == '=' || e.key == 'Enter'){
+        operate(true);
+    }
+    updateScreen();
+  }, true);
+
 logMemory = function(){
-    console.log("ls = " + lowerScreenDispaly + "\nstrd = " + storedNumber + "\nus = " + upperScreenDisplay + "\nop = " + op + "\ne = " + errorState);     //for testing purposes
+    console.log("ls = " + lowerScreenDisplay + "\nstrd = " + storedNumber + "\nus = " + upperScreenDisplay + "\nop = " + op + "\ne = " + errorState);     //for testing purposes
 }
 
 
